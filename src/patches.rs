3cc95adae3e2497be5c745f323064a68e7cb59f0
meanwhile-labs <178120528+meanwhile-labs@users.noreply.github.com>
2024-08-18 05:15:23 -0500

    Failing test for parsing

--- src/patches.rs
+++ src/patches.rs
@@ -1,11 +1,12 @@
-#[derive(Debug, PartialEq, Eq)]
+use serde::Deserialize;
+#[derive(Debug, PartialEq, Eq, Deserialize)]
 pub struct Patch {
     name: String,
     enabled: bool,
     patches: Vec<PatchEntry>,
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Deserialize)]
 pub struct PatchEntry {
     offset: u32,
     original: Vec<u8>,
@@ -30,5 +31,18 @@ mod test {
           # jmp LEGOStarWarsSaga.exe+CAD10; nop
           patch = "e9 3d 06 00 00 90"
         "#};
+        let patch: Patch = toml::from_str(input).unwrap();
+        assert_eq!(
+            patch,
+            Patch {
+                name: "Skip splash screens on startup".to_string(),
+                enabled: true,
+                patches: vec![PatchEntry {
+                    offset: 0xca6ce,
+                    original: vec![0x0f, 0x8a, 0x3c, 0x06, 0x00, 0x00],
+                    patch: vec![0xe9, 0x3d, 0x06, 0x00, 0x00, 0x90],
+                }]
+            }
+        );
     }
 }

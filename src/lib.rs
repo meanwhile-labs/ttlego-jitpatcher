4a928506bb03e605dc8827f0fa79e2be8f85caea
meanwhile-labs <178120528+meanwhile-labs@users.noreply.github.com>
2024-08-18 04:35:54 -0500

    Basically working

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,34 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use windows::{
+    core::{s, Error, PCSTR},
+    Win32::{
+        Foundation::{BOOL, FALSE, HINSTANCE, HWND, TRUE},
+        UI::WindowsAndMessaging::{MessageBoxA, MESSAGEBOX_STYLE},
+    },
+};
+
+#[no_mangle]
+pub extern "system" fn DllMain(_dll_module: HINSTANCE, call_reason: u32, _reserved: isize) -> BOOL {
+    const DLL_PROCESS_ATTACH: u32 = 1;
+
+    match call_reason {
+        DLL_PROCESS_ATTACH => match init() {
+            Ok(_) => TRUE,
+            Err(err) => {
+                unsafe {
+                    MessageBoxA(
+                        HWND::default(),
+                        PCSTR::from_raw(err.to_string().as_ptr()),
+                        s!("Error patching game"),
+                        MESSAGEBOX_STYLE(0x00000000),
+                    );
+                }
+                FALSE
+            }
+        },
+        _ => TRUE,
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+fn init() -> Result<(), Error> {
+    Ok(())
 }
